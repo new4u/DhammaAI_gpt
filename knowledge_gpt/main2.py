7c2d0236eda27a4540aa7d46c05d475e74acc1d3
Adam <zhangpei.adam@gmail.com>
2023-05-10 16:21:43 +0800

    Update main2.py

--- knowledge_gpt/main2.py
+++ knowledge_gpt/main2.py
@@ -34,82 +34,13 @@ hide_default_format = """
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
-sidebar()
 components.html(
    """
 <div id="customgpt_chat"></div>
 <script src="https://cdn.customgpt.ai/js/embed.js" width="100%" height="500px" p_id="1824" p_key="50a7b1a9e4e384f4b1284a5aa074770f" div_id="customgpt_chat"></script>
 	"""
 )
-uploaded_file = st.file_uploader(
-    "Upload a pdf, docx, or txt file 上传文件",
-    type=["pdf", "docx", "txt"],
-    help="Scanned documents are not supported yet! 扫描的文件不支持",
-    on_change=clear_submit,
-)
 
-index = None
-doc = None
-if uploaded_file is not None:
-    if uploaded_file.name.endswith(".pdf"):
-        doc = parse_pdf(uploaded_file)
-    elif uploaded_file.name.endswith(".docx"):
-        doc = parse_docx(uploaded_file)
-    elif uploaded_file.name.endswith(".txt"):
-        doc = parse_txt(uploaded_file)
-    else:
-        raise ValueError("File type not supported!")
-    text = text_to_docs(doc)
-    try:
-        with st.spinner("Indexing document... This may take a while⏳"):
-            index = embed_docs(text)
-        st.session_state["api_key_configured"] = True
-    except OpenAIError as e:
-        st.error(e._message)
-
-query = st.text_area("Ask a question about the document 对文件提问题", on_change=clear_submit)
-with st.expander("Advanced Options"):
-    show_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
-    show_full_doc = st.checkbox("Show parsed contents of the document")
-
-if show_full_doc and doc:
-    with st.expander("Document"):
-        # Hack to get around st.markdown rendering LaTeX
-        st.markdown(f"<p>{wrap_text_in_html(doc)}</p>", unsafe_allow_html=True)
-
-button = st.button("Submit 提交")
-if button or st.session_state.get("submit"):
-    if not st.session_state.get("api_key_configured"):
-        st.error("Please configure your OpenAI API key!")
-    elif not index:
-        st.error("Please upload a document!")
-    elif not query:
-        st.error("Please enter a question!")
-    else:
-        st.session_state["submit"] = True
-        # Output Columns
-        answer_col, sources_col = st.columns(2)
-        sources = search_docs(index, query)
-
-        try:
-            answer = get_answer(sources, query)
-            if not show_all_chunks:
-                # Get the sources for the answer
-                sources = get_sources(answer, sources)
-
-            with answer_col:
-                st.markdown("#### Answer")
-                st.markdown(answer["output_text"].split("SOURCES: ")[0])
-
-            with sources_col:
-                st.markdown("#### Sources")
-                for source in sources:
-                    st.markdown(source.page_content)
-                    st.markdown(source.metadata["source"])
-                    st.markdown("---")
-
-        except OpenAIError as e:
-            st.error(e._message)
 
 # Construct the file path dynamically
 #image_path = os.path.abspath("docgpt.jpg")
